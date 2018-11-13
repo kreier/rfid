bcf09069a260d097b2798078b72b8eb18787dd9c
Matthias Kreier <43933271+matthiaskreier@users.noreply.github.com>
2018-11-13 10:59:17 +0700

    Add files via upload

---
+++ arduino/display1602_helloworld.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h> // include i/o class header
+
+hd44780_I2Cexp lcd; // declare lcd object: auto locate & config display for hd44780 chip
+
+void setup()
+{
+  // initialize LCD with number of columns and rows:
+  lcd.begin(16, 2);
+
+  // Print a message to the LCD
+  lcd.print("Hello World!");
+  lcd.setCursor(0,1);
+}
+
+void loop()
+{
+  lcd.setCursor(11, 1);
+  lcd.print(millis() / 1000);
+  delay(1000);
+}
