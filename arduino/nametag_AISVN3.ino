5a3786d9c5548ef0e8164d9712123e9168aeb722
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2019-10-16 10:08:00 +0700

    progress bar added

--- arduino/nametag_AISVN3.ino
+++ arduino/nametag_AISVN3.ino
@@ -9,11 +9,12 @@ int data1 = 0;
 int ok = -1;
 int LED = 13;
 int y = 0;
+int block = 255;
 
 int tag1[14] = {2,48,49,48,70,65,66,49,70,52,50,70,55,3}; //mk
 int tag2[14] = {2,50,49,48,48,50,52,53,69,56,68,68,53,3}; //pl
 int tag3[14] = {2,50,49,48,48,50,52,52,54,55,56,51,65,3}; // Katie
-int tag4[14] = {2,50,49,48,48,50,52,55,70,55,52,48,68,3}; // Luke - Grandmaster of Procratination
+int tag4[14] = {2,50,49,48,48,50,52,55,70,55,52,48,68,3}; // Luke - Grandmaster of Procrastination
 int newtag[14] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // used for read comparisons
 
 hd44780_I2Cexp lcd; // declare lcd object: auto locate & config display for hd44780 chip
@@ -138,12 +139,24 @@ void readTags()
   }
 }
 
+void progressbar() {
+  lcd.setCursor(y, 1);
+  lcd.write(block);
+  y++;
+  if( y > 15 ) {
+    y = 0 ;
+    if (block == 255) {
+      block = 160;
+    }
+    else {
+      block = 255;
+    }
+  }
+}
+
 void loop()
 {
   readTags();
-  lcd.setCursor(y, 1);
-  lcd.print(" *");
-  y++;
+  progressbar();
   delay(100);
-  if( y > 16 ) y = 0 ;
 }
